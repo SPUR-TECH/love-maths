5157fb5940d8f006f9079390c1c56cdc7365e645
Scott Atkinson <scott.atkin79@gmail.com>
2021-12-08 13:20:01 +0000

    add code to increment score and wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("hey! you got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww...... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -72,11 +74,25 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get current score from the DOM and increase by 1
+ */
+
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Get current tally of incorrect answers from the DOM and increase by 1
+ */
+
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
